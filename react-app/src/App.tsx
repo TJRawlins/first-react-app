4c33ce9cab37ca49b54f6b9888db4dc76f5518e3
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2023-11-12 20:35:14 -0700

    added button component and updated task comp

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["New York", "Houston", "Phoenix", "Los Angeles"];
   const handleSelectItem = (item: string) => { console.log(item)}
+  
+  const [showAlert, setShowAlert] = useState(false);
+  
   return (
-  <div>
+  <>
       <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
-      <Alert>
-        Hello World
-      </Alert>
-  </div>)
+      {showAlert && <Alert onClose={()=> setShowAlert(false)}>My Alert</Alert>}
+      <Button color="danger" onClick={() => setShowAlert(true)}>
+        Click Me!
+      </Button>
+  </>
+  );
 }
 
 export default App;
--- react-app/src/components/Alert.tsx
+++ react-app/src/components/Alert.tsx
@@ -3,11 +3,21 @@ import { ReactNode } from "react";
 // children prop allows you to pass prop as a child to a component
 interface Props {
     children: ReactNode;
+    onClose: () => void;
 }
 
-const Alert = ({children}: Props) => {
+const Alert = ({children, onClose}: Props) => {
   return (
-    <div className="alert alert-primary">{children}</div>
+    <div className={"alert alert-primary alert-dismissible fade show"}>
+        {children}
+        <button 
+            onClick={onClose}
+            type="button" 
+            className="btn-close" 
+            data-bs-dismiss="alert" 
+            aria-label="Close">
+        </button>
+    </div>
   )
 }
 
